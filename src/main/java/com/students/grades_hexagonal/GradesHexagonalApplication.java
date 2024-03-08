80d44ed4e6efb93344ab7a70d1a29d978dba4807
juanjose0416 <juanjosesar04@gmail.com>
2024-03-08 10:40:42 -0500

    Inicialización paquetes del proyecto

---
+++ src/main/java/com/students/grades_hexagonal/GradesHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.students.grades_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/students/grades_hexagonal/GradesHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.students.grades_hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradesHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
